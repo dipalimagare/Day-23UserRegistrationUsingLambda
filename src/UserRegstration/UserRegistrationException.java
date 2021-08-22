4995b20d18ec9867be114fd75b091a6f831ffb6c
dipalimagare <dipalimagare2026@gmail.com>
2021-08-22 16:24:44 +0530

    Uploading UserRegistrationUsingLambda

---
+++ src/UserRegstration/UserRegistrationException.java
@@ -0,0 +1,17 @@
+package UserRegstration;
+
+public class UserRegistrationException extends Exception {
+	static String message;
+
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_INVALID
+	}
+
+	ExceptionType type;
+
+	public UserRegistrationException(ExceptionType type,String message)
+    {
+        super(message);
+        this.type = type;
+    }
+}
